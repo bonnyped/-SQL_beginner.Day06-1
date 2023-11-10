09c2085234809bfc69345041b16991a5f62fb0f2
bonnyped <bonnyped@student.21-school.ru>
2023-11-10 12:37:03 +0600

    minor changes

---
+++ src/ex02/day06_ex02.sql
@@ -0,0 +1,11 @@
+SELECT p.name,
+       mn.pizza_name,
+       mn.price,
+       (mn.price * (1 - (pd.discounts / 100))) AS disount_price,
+       zz.name AS pizzeria_name
+FROM person_order po
+         JOIN menu mn on po.menu_id = mn.id
+         JOIN person p on po.person_id = p.id
+         JOIN person_discounts pd on p.id = pd.person_id
+         JOIN pizzeria zz ON pd.pizzeria_id = zz.id
+ORDER BY 1, 2;
